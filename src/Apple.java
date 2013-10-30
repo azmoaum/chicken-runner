667b99a8a3213c3e29abf5da5d0f727f0fdf5bed
Andrew Aodh Zhao <aodhzhao@gmail.com>
2013-10-30 10:32:58 -0700

    Added Apple Class

---
+++ src/Apple.java
@@ -0,0 +1,37 @@
+import java.awt.Point;
+
+public class Apple {
+	private final int X_VELOCITY = 2;
+	private final int APPLE_START_Y = 125;
+	private final int APPLE_START_X = 800;
+	private Point point;
+	private boolean eaten;
+	
+	public Apple() {
+		this.point = new Point(APPLE_START_X, APPLE_START_Y);
+		this.eaten = false;
+	}
+
+	public Point getPoint() {
+		return this.point;
+	}
+
+	public void moveLeft() {
+		this.point.x -= X_VELOCITY;
+	}
+	
+	public void spawn() {
+		this.eaten = false;
+		this.point.x = APPLE_START_X;
+	}
+	
+	public boolean isEaten(){
+		return this.eaten;
+	}
+	
+	public void setEaten(boolean eaten) {
+		this.eaten = eaten;
+	}
+	
+	
+}
