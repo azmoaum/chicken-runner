919c12979a7a148a5b6731b9988e5bb7e173db94
Andrew Aodh Zhao <aodhzhao@gmail.com>
2013-10-31 09:26:56 -0700

    Removed a few methods

--- src/Apple.java
+++ src/Apple.java
@@ -4,12 +4,18 @@ public class Apple {
 	private final int X_VELOCITY = 2;
 	private final int APPLE_START_Y = 125;
 	private final int APPLE_START_X = 800;
+	private final int SPAWN_TIME = 8000;
+	
 	private Point point;
-	private boolean eaten;
+	//private boolean eaten;
 	
 	public Apple() {
 		this.point = new Point(APPLE_START_X, APPLE_START_Y);
-		this.eaten = false;
+		//this.eaten = false;
+	}
+	
+	public int getSpawnTime() {
+		return SPAWN_TIME;
 	}
 
 	public Point getPoint() {
@@ -20,18 +26,18 @@ public class Apple {
 		this.point.x -= X_VELOCITY;
 	}
 	
-	public void spawn() {
-		this.eaten = false;
-		this.point.x = APPLE_START_X;
-	}
+	//public void spawn() {
+		//this.eaten = false;
+	//	this.point.x = APPLE_START_X;
+	//}
 	
-	public boolean isEaten(){
+	/*public boolean isEaten(){
 		return this.eaten;
 	}
 	
 	public void setEaten(boolean eaten) {
 		this.eaten = eaten;
-	}
+	}*/
 	
 	
 }
