06db2583d4dc6ea1cd70a9f6ccc9682a65a8bfff
Andrew Aodh Zhao <aodhzhao@gmail.com>
2013-11-20 18:40:35 -0800

    Added Constants class

---
+++ src/Constants.java
@@ -0,0 +1,36 @@
+public final class Constants {
+        private Constants() {
+                throw new UnsupportedOperationException();
+        }
+        
+    	public static final int MAIN_TIMER_DELAY = 15;
+    	
+    	public static final int FRAME_WIDTH = 800;
+    	public static final int FRAME_HEIGHT = 400;
+    	public static final int FONT_SIZE = 20;
+    	
+    	public static final int BG_DX = 2;
+
+    	public static final int SCORE_X = 10;
+    	public static final int SCORE_Y = 20;
+    	
+    	public static final int CHICKEN_GRAVITY = 1;
+    	public static final int CHICKEN_START_Y = 300;
+    	public static final int CHICKEN_X_VELOCITY = 4;
+    	public static final int CHICKEN_Y_VELOCITY = 20;
+    	
+    	public static final int APPLE_SPAWN_DELAY = 1000;
+    	public static final int APPLE_SCORE_VALUE = 250;
+    	public static final int APPLE_X_VELOCITY = 2;
+    	public static final int APPLE_START_Y = 125;
+    	public static final int APPLE_START_X = 800;
+    	
+    	public static final int MISSLE_X_VELOCITY = 10;
+    	public static final int MISSLE_LENGTH = 20;
+    	public static final int MISSLE_Y_OFFSET = 30;
+    	
+    	public static final int ENEMY_X_VELOCITY = 2;
+    	public static final int ENEMY_START_X = 800;
+    	public static final int ENEMY_START_Y = 310;
+    	public static final int ENEMY_SPAWN_DELAY = 2000;
+}
