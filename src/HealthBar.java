5375e186e9388103879009f3d6cf4359dae817c0
Andrew Aodh Zhao <aodhzhao@gmail.com>
2013-11-21 08:49:13 -0800

    Added HealthBar class

---
+++ src/HealthBar.java
@@ -0,0 +1,38 @@
+import java.awt.Point;
+
+public class HealthBar {
+	private float health;
+	
+	public HealthBar() {
+		this.health = Constants.HEALTH_MAX;
+	}
+	
+	public void addHealth() {
+		if (this.health < 100) {
+			this.health += 1;
+		}
+	}
+	
+	public void removeHealth() {
+		if (this.health > 0) {
+			this.health -= 1;
+		}
+	}
+
+	public float getHealth() {
+		return this.health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getX() {
+		return Constants.HEALTHBAR_X + Constants.HEALTHBAR_WIDTH - (int) (this.health / Constants.HEALTH_MAX * Constants.HEALTHBAR_WIDTH);
+	}
+
+	public float getHealthPercentage() {
+		return this.health / Constants.HEALTH_MAX;
+	}
+	
+}
